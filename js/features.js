d0a862a2317617c07957a4a051622e87a6409122
waajdjdn <2706022403@qq.com>
2026-09-01 20:30:17 +0800

    Update audio source URL for keepalive feature
    修改已音频

--- js/features.js
+++ js/features.js
@@ -186,7 +186,7 @@
 
 (function() {
     var KEY = 'keepaliveAudioEnabled';
-    var SRC = 'https://img.heliar.top/file/1772885159972_silence.m4a';
+    var SRC = 'https://waajdjdn.github.io/zz0610/zz0610.m4a';
     var _audio = null;
     var _unlockBound = false;
 
